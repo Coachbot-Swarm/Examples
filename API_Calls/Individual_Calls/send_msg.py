4c5b42404bb5114536f7f2bec98124b995392ac7
CoachbotSwarm <coachbotswarmsystem@gmail.com>
2024-10-24 16:08:24 -0500

    Update send_msg.py

--- API_Calls/Individual_Calls/send_msg.py
+++ API_Calls/Individual_Calls/send_msg.py
@@ -7,23 +7,27 @@
 
     Parameters: msg should be a string that is less than 64 bytes or it will be truncated. This msg can be the output of the struct.pack() function explained in the section below.
     Output: True is successful, False if not
-    Example: robot.send_msg(struct.pack(‘fffii’, float_0, float_1, float_2, int_0, int_1))
+    Example: robot.send_msg(struct.pack(`fffii`, float_0, float_1, float_2, int_0, int_1))
 
     struct.pack() example:
-    struct.pack(‘fffii’, float_0, float_1, float_2, int_0, int_1)
+    struct.pack(`fffii`, float_0, float_1, float_2, int_0, int_1)
 
-    The first argument in the example above, ‘fffii’, specifies the type of variables being sent in what order and quantity. In this example, I 
+    The first argument in the example above, `fffii`, specifies the type of variables being sent in what order and quantity. In this example, I 
     am sending three floats and two signed integers. The remaining parameters are the corresponding variables I am sending in the message.
     This line would be called in the following way, where the struct_pack() function is called and passed into the send_msg() function: 
     
-    robot.send_msg(struct.pack(‘fffii’, float_0, float_1, float_2, int_0, int_1))
+    robot.send_msg(struct.pack(`fffii`, float_0, float_1, float_2, int_0, int_1))
 
     Expected behavior: The robot(s) will send a message and print it to the log file
 '''
 import struct
 
 def usr(robot):
-
+    log = open("experiment_log.txt", "w")
+    
+    log.write("an example write string\n")
+    log.flush()
+    
     id = robot.virtual_id()
 
     while True:
@@ -32,6 +36,11 @@ def usr(robot):
         for loop_count_variable in range(5):    # for 5 repetitions
             to_send = struct.pack('iif', id, loop_count_variable, robot.get_clock())    # create struct message
             robot.send_msg(to_send) # send message
+            log.write(str(id) + " sending loop count of " + str(loop_count_variable) + " at " + str(robot.get_clock()) + "\n")
+            log.flush()
             robot.delay(50) # add a short delay between sending messages for best results
 
+            robot.delay(1000) # delay for 1 second between each loop
+
+        log.close()
         return
